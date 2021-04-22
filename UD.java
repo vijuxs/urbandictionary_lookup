ad96ab466971d84467cf8dc0086976f82816674d
viju <vijuxs@users.noreply.github.com>
2021-04-22 19:45:10 +0530

    JSON parsed and printed.

--- UD.java
+++ UD.java
@@ -13,6 +13,9 @@ import java.net.URL;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.net.URLEncoder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.util.List;
 
 public class UD {
 
@@ -23,7 +26,7 @@ public class UD {
       System.out.println("Usage:> java UD <your phrase>");
       return;
     }
-
+    String rawJson = "";
     String phrase = convertToString(args);
     try {
         phrase = URLEncoder.encode(phrase, "UTF-8");
@@ -38,21 +41,50 @@ public class UD {
 
         String message = connection.getResponseMessage();
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
         String read = null;
         while ((read = reader.readLine()) != null) {
-          System.out.println(read);
+         
+          rawJson += read;
         }
       } catch (Exception e) {
         e.printStackTrace();
       }
+
+      EntryList entryList = readFromJson(rawJson);
+      System.out.println(entryList.getEntries());
+      List<Entry> entries = entryList.getEntries();
+      for (Entry e : entries) {
+       System.out.println(e.getDefId() + " : " + e.getDefinition());
+       System.out.println("----------------------------------------");
+      }
   }
 
-  public static String convertToString(String[] args) {
+  private static String convertToString(String[] args) {
     //TODO: any better way to write this?
     String words = "";
     for (String a : args) {
      words = words + " " + a;
     }
     return words.trim();
+  }
+
+  private static EntryList readFromJson(String rawJson) {
+    
+    if (rawJson == null || rawJson.equals("")) {
+      return null;
+    }
+
+    EntryList entries = new EntryList();
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      Entry entry = new Entry();
+      entries = mapper.readValue(rawJson, new TypeReference<EntryList>(){});
+      // entries = mapper.readValue(rawJson, EntryList.class);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return entries;
   }
 }
